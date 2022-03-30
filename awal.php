9b622d3dc60f2361b14c69b027bcd57efd78a696
endarstya <92735958+endarstya@users.noreply.github.com>
2022-03-30 10:20:10 +0700

    upload file awal

---
+++ awal.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="">
+    <title>Tugas Besar</title>
+    <style>
+
+body{
+    background: linear-gradient(to right, #f9f9f9 55%, #dee9ff 70%);
+    }
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 24px 30px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+img[name='wash']{
+    background-image: none;
+    width: 600px;
+    height: 500px;
+    margin: 75px;
+    position: fixed;
+}
+.font h1[name='bersih'], .font h1[name='rapi']{
+    margin: 0px 950px;
+    font-size: 80px;
+    padding: 10px 170px 20px;
+}
+
+.text p{
+   float: right;
+   font-size: 70px;
+   margin-right: 150px;
+   text-align: center;
+   margin-top: 200px;
+   font-family: fantasy;
+}
+
+</style>
+  </head>
+  <body>
+  <ul>
+  <li><a href="login.php">Login</a></li>
+  <!-- <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li> -->
+</ul>
+<!-- <div class="font">
+<h1 name='bersih'>Bersih</h1>
+    <h1 name='rapi'>Rapi</h1>
+</div> -->
+<div>
+    <img src="washhh.png" name="wash">
+</div>
+<div class="text">
+  <seaction>
+    <p>Pancarona <br> Car Wash</p>
+  </seaction>
+  </div
+
+
+  
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
